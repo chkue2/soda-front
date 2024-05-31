a8d28c8c20df830b599c75236bcee766d0eadef4
hangyu <chkue0924@lawendtech.co.kr>
2024-05-31 15:37:45 +0900

    은행 사용자 페이지 닫으면 인증정보 지움

--- pages/lawyer/detail/match/bank/[tid]/[id]/index.vue
+++ pages/lawyer/detail/match/bank/[tid]/[id]/index.vue
@@ -3,5 +3,14 @@
 </template>
 
 <script setup>
+import { onBeforeUnmount } from 'vue';
+
 import LawyerDetailPage from '~/components/page/LawyerDetailPage.vue';
+
+import { BANK_ID_KEY, BANK_AUTH_KEY } from '~/assets/js/storageKeys.js';
+
+onBeforeUnmount(() => {
+	window.localStorage.removeItem(BANK_ID_KEY);
+	window.localStorage.removeItem(BANK_AUTH_KEY);
+});
 </script>
--- pages/user/contract-manage/detail/bank/[id]/index.vue
+++ pages/user/contract-manage/detail/bank/[id]/index.vue
@@ -3,13 +3,21 @@
 </template>
 
 <script setup>
+import { onBeforeUnmount } from 'vue';
 import { useRoute } from 'vue-router';
 
 import ContractManageDetailPage from '~/components/page/ContractManageDetailPage.vue';
+
+import { BANK_ID_KEY, BANK_AUTH_KEY } from '~/assets/js/storageKeys.js';
 
 definePageMeta({
 	middleware: 'bank-auth',
 });
 
 const route = useRoute();
+
+onBeforeUnmount(() => {
+	window.localStorage.removeItem(BANK_ID_KEY);
+	window.localStorage.removeItem(BANK_AUTH_KEY);
+});
 </script>
