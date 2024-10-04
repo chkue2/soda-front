003e9c74059bd3740b4e002e2af1c83a53871030
hangyu <chkue0924@lawendtech.co.kr>
2024-10-04 16:38:01 +0900

    랜딩페이지 디자인 변경

--- pages/intro/soda/index.vue
+++ pages/intro/soda/index.vue
@@ -1,109 +1,113 @@
 <template>
-	<HeaderClose title="등기소다 A TO Z" />
-	<div class="intro-soda-title">
-		비용계산부터<br />법무사 매칭까지<br /><b>한번에</b>
-	</div>
-	<div class="intro-main-image-container">
-		<p class="intro-main-text">5개 항목만 입력하면<br />필요한 비용계산 완료</p>
-		<picture>
-			<source
-				srcset="/img/intro/soda-intro-mo-01.gif"
-				media="all and (max-width: 450px)"
-			/>
-			<img src="/img/intro/soda-intro-pc-01.gif" alt="인트로 이미지" />
-		</picture>
-	</div>
-	<section class="intro-soda-section pt24 pb76">
-		<p class="intro-soda-section-title">
-			복잡한 절차?<br /><b>소다가 한번에!</b>
-		</p>
-		<p class="intro-soda-section-content">
-			등기소다는 부동산 거래 과정에서 등기 절차를 보다 효율적으로 관리하고
-			법무사와 고객 간의 매칭을 원활하게 지원하는 어플리케이션입니다. 등기소다는
-			주로 고객이 부동산을 구매하거나 판매할 때 발생하는 소유권 이전 등기 절차를
-			관리하는 데 사용됩니다.
-		</p>
+	<HeaderClose title="등기소다 사용자가이드" />
+	<section class="intro-top-section">
+		<div class="intro-top-text-container">
+			<p class="intro-top-title">복잡한 절차?<br />등기소다로 한번에!</p>
+			<p class="intro-top-subtitle">
+				어려운 비용계산부터<br />매칭까지 원스톱서비스!
+			</p>
+		</div>
+		<div class="intro-top-image-container">
+			<img src="/img/icon/bank-house.png" />
+		</div>
 	</section>
-	<picture>
-		<source
-			srcset="/img/intro/soda-intro-mo-02.png"
-			media="all and (max-width: 450px)"
-		/>
-		<img src="/img/intro/soda-intro-pc-02.png" alt="인트로 이미지" />
-	</picture>
-	<div class="intro-soda-section pt80 pb71">
-		<p class="intro-soda-caption">HOW TO USE</p>
-		<p class="intro-soda-section-center-title">
-			<b>등기소다</b>는<br />어떻게 사용하나요?
+	<section class="intro-tip-section">
+		<p class="intro-tag mb10">소다 꿀팁</p>
+		<p class="intro-title mb10">1. 등기소다는 어떤 서비스인가요?</p>
+		<p class="intro-content mb40">
+			거래의 끝은 등기다! 라는 말을 들어보셨나요? 소유권이전등기가 그만큼
+			중요하다는 말인데요.소유권이전등기 절차는 복잡한 서류 준비와 법적 검토가
+			필요하므로 법무사의 도움을 받는 것이 중요합니다. 보통은 계약할 때
+			공인중개사의 소개로 법무사를 소개받지만 등기소다에서는 검증된
+			법무사,변호사를 선임할 수 있고 법적수수료 등 시장가를 비교하여 선택할 수
+			있습니다.
 		</p>
-		<p class="intro-soda-section-content center">
-			등기소다는 기존 복잡한 구조에서 벗어나 편리한 사용성을 추구함에 있어
-			누구나 쉽고 빠르게 이용할 수 있습니다.
+		<p class="intro-title mb10">2. 등기프로는 뭔가요?</p>
+		<p class="intro-content mb40">
+			등기프로는 ‘법무사' 혹은 ‘변호사’등 고객님의 소유권이전등기를 책임질
+			전문가를 뜻합니다.
 		</p>
-		<div class="intro-soda-cow">
-			<img src="/img/cow/cow-02.png" alt="인트로 이미지" />
+		<p class="intro-title mb10">3. 등기소다를 어떻게 사용하면 좋을까요?</p>
+		<p class="intro-content mb40">
+			<b class="intro-blue">[계산하기]</b> 기능은 견적을 넣기전에
+			소유권이전등기에 필요한 가금액을 알 수 있습니다.<br />
+			[홈] > [계산하기]로 접근할 수 있고 하단버튼에서 두번째 영수증 아이콘을
+			누르면 [계산하기] 페이지로 이동됩니다.
+		</p>
+		<p class="intro-content mb40">
+			<b class="intro-blue">[등기프로 찾기]</b> 기능은 거래대상 주소지 주변의
+			등기프로를 찾을 수 있는 기능입니다.<br />
+			등기프로의 상세프로필을 확인할 수 있으며 리뷰,평점등이 노출되어있고 직접
+			연락하여 소통할 수 있습니다.<br />
+			[홈] > [등기프로 찾기]로 접근할 수 있고 하단버튼에서 세번째 돋보기
+			아이콘을 누르면 [찾기]페이지로 이동됩니다.
+		</p>
+		<p class="intro-content mb40">
+			<b class="intro-blue">[내 계약 관리하기]</b> 기능은 간단한 거래정보를
+			입력하면, 선착순으로 5명의 등기프로를 모집할 수 있습니다. 그 중 한 명을
+			선택하면, 해당 전문가가 소유권이전등기 업무를 정확하고 친절하게
+			처리해줍니다.
+		</p>
+		<div class="intro-tip-image-container pb50">
+			<img src="/img/cow/cow-folder.png" />
 		</div>
-		<p class="intro-soda-subtitle">1. 등기소다에 접속</p>
-		<p class="intro-soda-section-content">
-			원활한 서비스이용을 위해 회원가입 후, 등기소다에서 제공하는 간단한
-			입력폼에 정보를 입력해주세요.
-		</p>
-	</div>
-	<picture>
-		<source
-			srcset="/img/intro/soda-intro-mo-03.png"
-			media="all and (max-width: 450px)"
-		/>
-		<img src="/img/intro/soda-intro-pc-03.png" alt="인트로 이미지" />
-	</picture>
-	<div class="intro-soda-section pt24 pb60">
-		<p class="intro-soda-subtitle">2. 간단한 정보로 계산하기</p>
-		<p class="intro-soda-section-content">
-			등기소다에서 제공하는 간단한 입력폼에 정보를 기입해주세요.
-			소유권이전등기에 필요한 비용을 미리 계산해볼 수 있어요.
-		</p>
-	</div>
-	<picture>
-		<source
-			srcset="/img/intro/soda-intro-mo-09.png"
-			media="all and (max-width: 450px)"
-		/>
-		<img src="/img/intro/soda-intro-pc-09.png" alt="인트로 이미지" />
-	</picture>
-	<div class="intro-soda-section pt24 pb36">
-		<p class="intro-soda-subtitle">3. 견적넣기</p>
-		<p class="intro-soda-section-content">
-			계산해본 비용을 비교해본 후, 마음에 드는 상품의 유형을 선택하여
-			소유권이전등기에 필요한 전문가를 찾아보세요.
-		</p>
-		<div class="intro-soda-cow">
-			<img src="/img/cow/cow-03.png" alt="인트로 이미지" />
+		<div class="intro-tip-end mt50 mb50">
+			<p class="intro-tag">사용방법</p>
 		</div>
-	</div>
-	<div class="intro-soda-section pt24 pb21">
-		<p class="intro-soda-subtitle">4. 등기프로 매칭</p>
-		<p class="intro-soda-section-content">
-			견적을 넣으면 총 5명의 등기프로와 매칭이 시작됩니다. 등기프로의 평점이나
-			리뷰를 확인해보고 마음에 드는 등기프로가 없다면 다시찾기로 5명의
-			등기프로를 새로 추천받을 수 있어요.
+	</section>
+	<section class="intro-contents-section pb50">
+		<p class="contents-title">1. 내 계약 등록하기</p>
+		<p class="contents-subtitle">홈 > 내계약 관리하기 > 등록하기 버튼 클릭</p>
+		<p class="contents-content">
+			잔금일자, 목적물 소재지, 매매대금, 계약서 사진등<br />필수항목에 대한
+			정보를 입력후 다음을 눌러주세요
 		</p>
-	</div>
-	<picture>
-		<source
-			srcset="/img/intro/soda-intro-mo-07.png"
-			media="all and (max-width: 450px)"
-		/>
-		<img src="/img/intro/soda-intro-pc-07.png" alt="인트로 이미지" />
-	</picture>
-	<div class="intro-soda-section pt25 pb54">
-		<p class="intro-soda-section-content">
-			등기소다는 엄선된 전문 프로들로 구성된 네트워크를 자랑합니다. 우리
-			플랫폼에 등록된 모든 법무사들은 엄격한 심사 과정을 거쳐 선별되었으며,
-			전문성과 경험이 뛰어난 실력있는 전문가들입니다. 등기소다를 통해 고객들은
-			최고 수준의 법률 서비스를 제공받을 수 있습니다. 또한 등기소다는 법무사들의
-			실적과 평가를 투명하게 제공하여 고객이 신뢰할 수 있는 전문가를 선택할 수
-			있도록 지원합니다. 함께 등기소다를 통해 부동산 거래를 안전하게 진행해
-			보세요!
+		<div class="contents-image-container">
+			<img src="/img/intro/intro-01.png" />
+		</div>
+	</section>
+	<section class="intro-contents-section section--blue pt70 pb35">
+		<p class="contents-title">2. 서비스유형 선택 후, 등기프로 모집하기</p>
+		<p class="contents-subtitle">계약정보와 서비스유형에 맞는 금액 안내</p>
+		<p class="contents-content">
+			총 4가지의 서비스유형 중 하나를 선택하면<br />등기프로 모집이 시작됩니다.
+		</p>
+		<div class="contents-image-container">
+			<img src="/img/intro/intro-02.png" />
+		</div>
+	</section>
+	<section class="intro-contents-section pt70 pb95">
+		<p class="contents-title">3. 모집완료 후 등기프로 선택하기</p>
+		<p class="contents-subtitle">알림톡 > 등기프로 선택하기</p>
+		<p class="contents-content">
+			모집완료 알림톡을 받으면 등기프로선택하기로 접속,<br />모집에 참여한
+			등기프로 중 한명을 선택해주세요
+		</p>
+		<div class="contents-image-container">
+			<img src="/img/intro/intro-03.png" />
+		</div>
+	</section>
+	<section class="intro-contents-section section--blue pt70 pb35">
+		<p class="contents-title">4. 내 계약 진행현황 확인하기</p>
+		<p class="contents-subtitle">
+			등기프로 배정 후, 내계약관리 상세페이지 접근
+		</p>
+		<p class="contents-content">
+			목적물 소재지, 매매대금, 잔금일, 잔금시간 및<br />선택된 사무소와
+			진행현황을 확인할 수 있습니다.
+		</p>
+		<div class="contents-image-container">
+			<img src="/img/intro/intro-04.png" />
+		</div>
+	</section>
+	<div class="intro-tip-section">
+		<p class="intro-tag mb10">끝으로</p>
+		<p class="intro-title mb10">등기 잘하는 등기소다</p>
+		<p class="intro-content">
+			등기소다는 고객님들이 쉽고 편리하게 소유권이전등기 절차를 완료할 수 있도록
+			최선을 다하고 있습니다. 믿을 수 있는 전문가와 함께 안전하고 만족스러운
+			계약을 진행하시길 바라며, 앞으로도 고객님들의 성공적인 거래를 위해
+			끊임없이 노력하겠습니다.
 		</p>
 	</div>
 	<div class="intro-soda-section intro-soda-button-section pt41 pb36">
@@ -118,86 +122,116 @@ import HeaderClose from '~/components/layout/HeaderClose.vue';
 </script>
 
 <style lang="scss" scoped>
-.intro-soda-title {
-	margin: 55px 0 10px;
-	font-size: 34px;
+.intro-top-section {
+	background-color: #445feb;
+	height: 389px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 24px 0 130px;
+}
+.intro-top-text-container {
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+}
+.intro-top-title {
+	font-size: 24px;
 	font-weight: $ft-bold;
-	text-align: center;
-	& > b {
-		color: #29cdff;
-	}
+	line-height: 35px;
+	color: #ffffff;
 }
-.intro-main-image-container {
-	position: relative;
+.intro-top-subtitle {
+	font-size: 14px;
+	line-height: 20px;
+	color: #ffffff;
 }
-.intro-main-text {
-	font-size: 18px;
-	color: #878787;
-	text-align: center;
-	position: absolute;
-	top: 20px;
-	left: 50%;
-	transform: translateX(-50%);
-}
-picture {
-	width: 100%;
-	height: auto;
-	img {
-		width: 100%;
+.intro-top-image-container {
+	display: flex;
+	justify-content: flex-end;
+	& > img {
+		width: 189px;
 		height: auto;
 	}
 }
-.intro-soda-section {
-	padding: 0 25px;
+.intro-tip-section {
+	padding: 40px 24px 50px;
 }
-.intro-soda-section-title {
-	font-size: 28px;
+.intro-tag {
+	display: inline-block;
+	padding: 7px 11px;
+	font-size: 14px;
 	font-weight: $ft-medium;
-	& > b {
+	background-color: #e3e4e6;
+	border-radius: 30px;
+}
+.intro-title {
+	font-size: 17px;
+	font-weight: $ft-bold;
+}
+.intro-content {
+	color: #4f4f4f;
+	line-height: 28px;
+	.intro-blue {
 		font-weight: $ft-bold;
 		color: #29cdff;
 	}
 }
-.intro-soda-section-content {
-	color: #4f4f4f;
-	margin-top: 20px;
-	word-break: keep-all;
-	line-height: 1.45;
-	&.center {
-		text-align: center;
-	}
-}
-.intro-soda-caption {
-	font-weight: $ft-bold;
-	text-align: center;
-}
-.intro-soda-section-center-title {
-	margin-top: 11px;
-	font-size: 30px;
-	font-weight: $ft-bold;
-	text-align: center;
-	& > b {
-		color: #29cdff;
-	}
-}
-.intro-soda-cow {
+.intro-tip-image-container {
+	width: 100%;
 	display: flex;
 	justify-content: center;
-	margin: 114px 0 60px;
-	img {
-		width: 180px;
+	align-items: center;
+	border-bottom: 1px solid #dedede;
+	max-width: 310px;
+	margin: 0 auto;
+	& > img {
+		width: 152px;
 		height: auto;
 	}
 }
-.intro-soda-subtitle {
-	font-size: 20px;
-	color: #29cdff;
+.intro-tip-end {
+	display: flex;
+	justify-content: center;
+}
+.intro-contents-section {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 10px;
+	&.section--blue {
+		background-color: #f5faff;
+	}
+}
+.contents-title {
+	font-size: 17px;
 	font-weight: $ft-bold;
-	margin: 12px 0 10px;
+	text-align: center;
+}
+.contents-subtitle {
+	color: #4f4f4f;
+	text-align: center;
+}
+.contents-content {
+	font-size: 14px;
+	line-height: 25px;
+	color: #929292;
+	text-align: center;
+}
+.contents-image-container {
+	display: flex;
+	justify-content: center;
+	& > img {
+		width: 248px;
+		height: auto;
+	}
 }
 .intro-soda-button-section {
 	position: sticky;
 	bottom: 0;
+	padding: 0 24px;
 }
 .intro-soda-button {
 	display: flex;
@@ -212,5 +246,14 @@ picture {
 	font-weight: $ft-bold;
 	color: #ffffff;
 	text-decoration: none;
+}
+
+@media all and (max-width: 640px) {
+	.intro-top-section {
+		flex-direction: column;
+		justify-content: center;
+		align-items: unset;
+		padding: 0 24px;
+	}
 }
 </style>
